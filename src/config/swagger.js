32adafd96618c36b5967e7579a7c987eac7ab80c
inamnurulf <aam.inam7310@gmail.com>
2025-03-23 20:50:30 +0700

    feat: add swagger setup

---
+++ src/config/swagger.js
@@ -0,0 +1,28 @@
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "PMS API",
+      version: "1.0.0",
+      description: "API documentation for the PMS service",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+        description: "Local server",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"], // Automatically scan all route files
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+function setupSwagger(app) {
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+}
+
+module.exports = setupSwagger;
